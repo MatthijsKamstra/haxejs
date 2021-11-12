ac96f55cd0e2058816c71c6818e40378ae4f819a
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-11-12 20:23:57 +0100

    more comments

--- docs/21dts2hx/code/bin/sortablejs.js
+++ docs/21dts2hx/code/bin/sortablejs.js
@@ -1,23 +1,27 @@
 (function ($global) { "use strict";
-var Main = function() {
-	console.log("src/Main.hx:5:","SortableJs");
-	var el = window.document.getElementById("example1");
-	var sortable = new Sortable(el,{ animation : 150, ghostClass : "bg-danger"});
-};
-Main.main = function() {
-	var main = new Main();
-};
-var haxe_iterators_ArrayIterator = function(array) {
-	this.current = 0;
-	this.array = array;
-};
-haxe_iterators_ArrayIterator.prototype = {
-	hasNext: function() {
+class Main {
+	constructor() {
+		console.log("src/Main.hx:5:","SortableJs");
+		let el = window.document.getElementById("example1");
+		let sortable = new Sortable(el,{ animation : 150, ghostClass : "bg-danger"});
+	}
+	static main() {
+		let main = new Main();
+	}
+}
+class haxe_iterators_ArrayIterator {
+	constructor(array) {
+		this.current = 0;
+		this.array = array;
+	}
+	hasNext() {
 		return this.current < this.array.length;
 	}
-	,next: function() {
+	next() {
 		return this.array[this.current++];
 	}
-};
+}
+{
+}
 Main.main();
 })({});
--- docs/21dts2hx/code/dts2hx_convert.md
+++ docs/21dts2hx/code/dts2hx_convert.md
@@ -16,3 +16,14 @@ haxelib install hxnodejs
 # don't forget to add `--library sortablejs` to your build
 ```
 
+## small adjustments
+
+```haxe
+// @:jsRequire("sortablejs")
+@:native('Sortable')
+```
+
+remove `@:jsRequire("sortablejs")` and add `@:native('Sortable')`
+
+1. we will use the embedded js in the html
+2. we want to make sure it uses the name `Sortable` and not `Sortablejs`
