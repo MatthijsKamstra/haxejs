d41f31dd7ff70188b2e33d37f53779d118a2f56e
Matthijs Kamstra <matthijs@fonk.amsterdam>
2021-11-12 20:16:50 +0100

    adjustments to externs

--- docs/21dts2hx/code/.haxelib/sortablejs/1,10,7/Sortablejs.hx
+++ docs/21dts2hx/code/.haxelib/sortablejs/1,10,7/Sortablejs.hx
@@ -1,64 +1,81 @@
-@:jsRequire("sortablejs") extern class Sortablejs {
+// @:jsRequire("sortablejs")
+@:native('Sortable')
+extern class Sortablejs {
 	/**
 		Sortable's main constructor.
 	**/
 	function new(element:js.html.Element, options:sortablejs.Options);
-	public var options : sortablejs.Options;
-	public var el : js.html.Element;
+
+	public var options:sortablejs.Options;
+	public var el:js.html.Element;
+
 	/**
 		Options getter/setter
 	**/
-	@:overload(function<K>(name:K):Dynamic { })
+	@:overload(function<K>(name:K):Dynamic {})
 	function option<K>(name:K, value:Dynamic):Void;
+
 	/**
 		For each element in the set, get the first element that matches the selector by testing the element itself and traversing up through its ancestors in the DOM tree.
 	**/
 	function closest(element:js.html.Element, ?selector:String):Null<js.html.Element>;
+
 	/**
 		Sorts the elements according to the array.
 	**/
 	function sort(order:haxe.ds.ReadOnlyArray<String>):Void;
+
 	/**
 		Saving and restoring of the sort.
 	**/
 	function save():Void;
+
 	/**
 		Removes the sortable functionality completely.
 	**/
 	function destroy():Void;
+
 	/**
 		Serializes the sortable's item data-id's (dataIdAttr option) into an array of string.
 	**/
 	function toArray():Array<String>;
-	static var prototype : Sortablejs;
-	static var active : Null<Sortablejs>;
-	static var utils : sortablejs.Utils;
+
+	static var prototype:Sortablejs;
+	static var active:Null<Sortablejs>;
+	static var utils:sortablejs.Utils;
+
 	/**
 		Mounts a plugin to Sortable
 	**/
 	static function mount(sortablePlugins:haxe.extern.Rest<sortablejs.plugins.SortablePlugin>):Void;
+
 	/**
 		Creation of new instances.
 	**/
 	static function create(element:js.html.Element, ?options:sortablejs.Options):Sortablejs;
+
 	/**
 		The element being dragged.
 	**/
-	static var dragged : Null<js.html.Element>;
+	static var dragged:Null<js.html.Element>;
+
 	/**
 		The ghost element.
 	**/
-	static var ghost : Null<js.html.Element>;
+	static var ghost:Null<js.html.Element>;
+
 	/**
 		The clone element.
 	**/
-	static var clone : Null<js.html.Element>;
+	static var clone:Null<js.html.Element>;
+
 	/**
 		Get the Sortable instance on an element.
 	**/
 	static function get(element:js.html.Element):Null<Sortablejs>;
+
 	/**
 		Get the Sortable version
 	**/
-	static final version : String;
+	static final version:String;
 }
--- docs/21dts2hx/code/bin/sortablejs.js
+++ docs/21dts2hx/code/bin/sortablejs.js
@@ -2,12 +2,11 @@
 var Main = function() {
 	console.log("src/Main.hx:5:","SortableJs");
 	var el = window.document.getElementById("example1");
-	var sortable = new Sortablejs(el,{ animation : 150, ghostClass : "bg-danger"});
+	var sortable = new Sortable(el,{ animation : 150, ghostClass : "bg-danger"});
 };
 Main.main = function() {
 	var main = new Main();
 };
-var Sortablejs = require("sortablejs");
 var haxe_iterators_ArrayIterator = function(array) {
 	this.current = 0;
 	this.array = array;
